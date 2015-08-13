e5ebf2a8c5ad12a24e649fc1ed88b34c354aa643
Tom Brown <herestomwiththeweather@gmail.com>
2015-08-13 13:12:54 -0500

    using SESSION_SECRET env var, verify session data for incoming client connection

--- server.rb
+++ server.rb
@@ -1,4 +1,6 @@
 require 'rubame'
+require 'openssl'
+require 'uri'
 
 module Rubame
   class Server
@@ -6,20 +8,88 @@ module Rubame
       @clients
     end
   end
+
+  class Client
+    def secure_compare(a, b)
+      # from Rack::Utils
+      #
+      # Constant time string comparison.
+      #
+      # NOTE: the values compared should be of fixed length, such as strings
+      # that have already been processed by HMAC. This should not be used
+      # on variable length plaintext strings because it could leak length info
+      # via timing attacks.
+      return false unless a.bytesize == b.bytesize
+
+      l = a.unpack("C*")
+
+      r, i = 0, -1
+      b.each_byte { |v| r |= v ^ l[i+=1] }
+      r == 0
+    end
+
+    def verify_session_data(session_data)
+      # from Rack::Session::Cookie
+      digest, session_data = session_data.reverse.split("--", 2)
+      digest.reverse! if digest
+      session_data.reverse! if session_data
+      session_data = nil unless digest_match?(session_data, digest)
+      if session_data.nil?
+        puts "XXX digest_match failed!"
+        false
+      else
+        puts "XXX session data verified!!!"
+        #coder.decode(session_data) || {}
+        true
+      end
+    end
+
+    def generate_hmac(data, secret)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret, data)
+    end
+
+    def digest_match?(data, digest)
+      return unless data && digest
+      secure_compare(digest, generate_hmac(data, ENV['SESSION_SECRET']))
+    end
+  end
 end
 
 server = Rubame::Server.new("0.0.0.0", 8080)
 
+connections = {}
+
 while true
   server.run do |client|
     client.onopen do
-      puts "client open: #{server.clients.length}"
+      session_data = ''
+      puts "client open: socket: #{client.socket} length: #{server.clients.length}"
+      puts "-----------------------------------------------------------"
+      puts "-                   Handshake                             -"
+      puts "-----------------------------------------------------------"
+      puts "Version: #{client.handshake.version}"
+      client.handshake.headers.each_pair do |key, value|
+        if 'cookie' == key
+          cookies = value.split
+          cookies.each do |c|
+            k,v = c.split('=')
+            if 'tjs-session' == k
+              session_data = v
+              puts "session_data : #{session_data}"
+            end
+          end
+        end
+      end
+
+      server.close(client) unless client.verify_session_data URI.unescape(session_data)
+
+      puts "-----------------------------------------------------------"
     end
 
     client.onmessage do |mess|
-      puts "message: #{mess}"
+      puts "message(#{client.socket}): #{mess}"
       server.clients.values.each do |c|
-        c.send mess
+        c.send mess unless c.socket == client.socket
       end
     end
 
